5677909fefc6284028e08196265ea083d70bdb02
Saifur Rahman Rubayet <saifur.rubayet@northsouth.edu>
2026-09-17 01:08:27 +0600

    feat: Update CORS configuration to restrict allowed origins to the production domain

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://web.shantaplastic.com'],
 
     'allowed_origins_patterns' => [],
 
